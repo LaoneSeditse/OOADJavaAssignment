39997b0b4fba3138e2c69fe7d3004baccd1c7f62
LaoneSeditse <cse24-125@thuto.bac.ac.bw>
2025-10-16 09:33:12 +0200

    fixed structure

---
+++ java_assignment/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.java_assignment {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.java_assignment to javafx.fxml;
+    exports com.example.java_assignment;
+}
